8c4f5c9a9f1e9fa6a88dbd87449b22b637a3e2e3
Alice Brookes <acb1g20@soton.ac.uk>
2024-12-07 16:55:39 +0000

    day1 challenge

---
+++ day1/day1.hs
@@ -0,0 +1,13 @@
+import System.IO
+import Data.List
+
+parseInput :: String -> ([Int], [Int])
+parseInput input =
+    let pairs = map ((\[a, b] -> (read a, read b)) . words) (lines input)
+    in unzip pairs
+
+main :: IO ()
+main = do
+    input <- readFile "input.txt"
+    let (list1, list2) = parseInput input
+    print . sum . map abs $ zipWith (-) (sort list1) (sort list2)
